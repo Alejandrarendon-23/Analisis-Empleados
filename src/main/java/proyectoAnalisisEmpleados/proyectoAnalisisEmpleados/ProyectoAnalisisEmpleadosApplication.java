ebca5de39896e980734ad9c84736439ba10d44ac
Alejandrarendon-23 <aleja.23121@gmail.com>
2018-03-05 09:31:38 -0500

    Inicio proyecto

---
+++ src/main/java/proyectoAnalisisEmpleados/proyectoAnalisisEmpleados/ProyectoAnalisisEmpleadosApplication.java
@@ -0,0 +1,12 @@
+package proyectoAnalisisEmpleados.proyectoAnalisisEmpleados;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoAnalisisEmpleadosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoAnalisisEmpleadosApplication.class, args);
+	}
+}
